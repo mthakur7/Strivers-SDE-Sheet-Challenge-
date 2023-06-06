63447e3ed76fa91d75f7a20b62868f8ab8446ad1
Manju Thakur <89182004+mthakur7@users.noreply.github.com>
2023-06-06 14:10:37 +0530

    Create 89-Next Smaller Element.cpp

---
+++ 89-Next Smaller Element.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+   vector<int>v;
+        stack<int>st;
+        for(int i=arr.size()-1;i>=0;i--){
+            if(st.empty())
+                v.push_back(-1);             
+                      
+                else{
+                    while(!st.empty() && st.top()>=arr[i])
+                        st.pop();
+                    if(!st.empty())
+                     v.push_back(st.top());
+                   else
+                       v.push_back(-1); 
+                }
+            st.push(arr[i]);
+            }
+        reverse(v.begin(),v.end());
+    
+        return v;
+    
+}
