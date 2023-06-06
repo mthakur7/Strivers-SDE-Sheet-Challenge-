98c1b3d364e2cce586bebfd544e56e00307c905b
Manju Thakur <89182004+mthakur7@users.noreply.github.com>
2023-06-07 03:34:20 +0530

    Create 126-Check Identical.cpp

---
+++ 126-Check Identical.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+
+    class BinaryTreeNode {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+		
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+***********************************************************/
+
+bool identicalTrees(BinaryTreeNode<int>* p, BinaryTreeNode<int>* q) {
+    // Write your code here. 
+         if(!p && !q)return true;
+        if(p && !q || q&&!p)return false;
+       
+        
+        if(p->data==q->data && identicalTrees(p->left,q->left) && identicalTrees(p->right,q->right))
+            return true;
+        else
+            return false;	 
+}
