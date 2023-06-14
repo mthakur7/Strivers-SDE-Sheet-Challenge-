0bd90a4d6e9adfbde2710d25fa25ae63c8d6f8c0
Manju Thakur <89182004+mthakur7@users.noreply.github.com>
2023-06-14 20:38:45 +0530

    Create 61-Find nth root of m.cpp

---
+++ 61-Find nth root of m.cpp
@@ -0,0 +1,15 @@
+int NthRoot(int n, int m) {
+  // Write your code here.'
+  int i=1,j=m;
+  while (i<=j) {
+    int mid = i + (j - i) / 2;
+    if (pow(mid, n) == m)
+      return mid;
+    else if (pow(mid, n) > m)
+      j = mid - 1;
+    else
+      i = mid + 1;
+  }
+  return -1;
+  
+}
