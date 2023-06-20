2b6cd4afe356bb18a895e3c805af345caa836c2e
Manju Thakur <89182004+mthakur7@users.noreply.github.com>
2023-06-20 23:19:28 +0530

    Create 30-delete node in linkedlist.cpp

---
+++ 30-delete node in linkedlist.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void deleteNode(LinkedListNode<int> * node) {
+    // Write your code here.
+     LinkedListNode<int>*temp=node->next;
+      
+        int x=node->data;
+        node->data=temp->data;
+        temp->data=x;
+        
+        node->next=temp->next;
+}
