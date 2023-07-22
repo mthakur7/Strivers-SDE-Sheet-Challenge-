92689b5985ad5c6e5d1d1bc43de9213d1ba97248
Manju Thakur <89182004+mthakur7@users.noreply.github.com>
2023-07-22 20:28:12 +0530

    Create 10-merge intervals.cpp

---
+++ 10-merge intervals.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+/*
+
+    intervals[i][0] = start point of i'th interval
+    intervals[i][1] = finish point of i'th interval
+
+*/
+
+vector<vector<int>> mergeIntervals(vector<vector<int>> &arr)
+{
+    sort(arr.begin(), arr.end());
+   int n=arr.size();
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++) {
+        // if the current interval does not
+        // lie in the last interval:
+        if (ans.empty() || arr[i][0] > ans.back()[1]) {
+            ans.push_back(arr[i]);
+        }
+        // if the current interval
+        // lies in the last interval:
+        else {
+            ans.back()[1] = max(ans.back()[1], arr[i][1]);
+        }
+    }
+    return ans;
+    // Write your code here.
+}
